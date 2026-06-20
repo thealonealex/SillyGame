b5cc9664a7326d8352af5ae8d1fadee51e85d9ab
AloneAlex <98485800+AloneAlexandros@users.noreply.github.com>
2026-06-20 18:14:45 +0300

    Added coyote time and wall jumping
    The implementation is VERY weird but it works :D

--- Assets/Scripts/PrototypeMovement.cs
+++ Assets/Scripts/PrototypeMovement.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
@@ -7,10 +10,13 @@ public class PrototypeMovement : MonoBehaviour
     [SerializeField] private float jumpMagnitude;
     private Rigidbody2D _rb;
 
-    private bool _canJump = false;
+    private string _currentlyTouching;
     private bool _jumpPressed = false;
     private Vector2 _moveDirection;
     [SerializeField] private float speed;
+    private Vector2 _walljumpDirection;
+
+    [SerializeField] private float coyoteTime = 0.1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,23 +24,41 @@ public class PrototypeMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (_jumpPressed && _canJump)
+        if (_jumpPressed && _currentlyTouching == "Ground")
         {
             //remove previous possible forces
             _rb.linearVelocity = Vector3.zero;
             _rb.angularVelocity = 0;
             //jump
             _rb.AddForce(Vector2.up * jumpMagnitude);
-            _canJump = false;
+            _jumpPressed = false;
+        }if (_jumpPressed && _currentlyTouching == "Wall")
+        {
+            //do a backjolt
+            _walljumpDirection = -_moveDirection;
+            StartCoroutine(Walljump());
+            _rb.AddForce(Vector2.up * jumpMagnitude);
+            _jumpPressed = false;
         }
-        transform.Translate(_moveDirection * (speed * Time.deltaTime));
+
+        if (_walljumpDirection != Vector2.zero)
+        {
+            _walljumpDirection = _walljumpDirection + _moveDirection * (Time.deltaTime * 5);
+        }
+        _rb.linearVelocity = _walljumpDirection != Vector2.zero && _moveDirection != Vector2.zero ? new Vector2(_walljumpDirection.x * speed, _rb.linearVelocity.y) : new Vector2(_moveDirection.x * speed, _rb.linearVelocity.y);
     }
 
+    private IEnumerator Walljump()
+    {
+        yield return new WaitForSeconds(0.2f);
+        _walljumpDirection = Vector2.zero;
+    }
     public void Jump(InputAction.CallbackContext context)
     {
         //todo: coyote timing
+        //todo: extra force during walljumping
         if (context.performed)
         {
             _jumpPressed = true;
@@ -46,10 +70,24 @@ public class PrototypeMovement : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (_currentlyTouching == "Ground" && collision.gameObject.CompareTag("Wall")) return;
+        _currentlyTouching = collision.gameObject.tag;
+        Debug.Log(_currentlyTouching);
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(_currentlyTouching))
         {
-            _canJump = true;
+            StartCoroutine(CoyoteTime("Nothing"));
         }
+        Debug.Log(_currentlyTouching);
+    }
+
+    private IEnumerator CoyoteTime(string Tag)
+    {
+        yield return new WaitForSeconds(coyoteTime);
+        _currentlyTouching = Tag;
     }
 
     public void Move(InputAction.CallbackContext context)
